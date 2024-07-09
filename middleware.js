4bef75b530d2ccc92a38137a754a8754ef71a90e
GowrisankarKumaresan <Gowrisankar.Kumaresan@lumen.com>
2024-07-09 17:08:19 +0530

    dynamic url added

--- middleware.js
+++ middleware.js
@@ -11,7 +11,7 @@ const Middleware = (req) => {
   const roourl = isProd ? 'https://talent-api-integration-dasboard.vercel.app/' : 'http://localhost:3000'
   if (pathName.toLowerCase() === "/login") {
     if (cookieData && cookieData?.value) {
-      return NextResponse.redirect("http://localhost:3000");
+      return NextResponse.redirect(roourl);
     }
   }
 
@@ -27,12 +27,12 @@ const Middleware = (req) => {
       let parsedEndTime = parseDateString(endTime);
       if (decoded && parsedBeginningTime > parsedEndTime) {
         req.cookies.has(NEXT_TOKEN) && req.cookies.delete(NEXT_TOKEN);
-        return NextResponse.redirect("http://localhost:3000" + "/login");
+        return NextResponse.redirect(roourl + "/login");
       } else {
         return NextResponse.next();
       }
     } else {
-      return NextResponse.redirect("http://localhost:3000" + "/login");
+      return NextResponse.redirect(roourl + "/login");
     }
   }
   return NextResponse.next();
